fa534df68447670f4d6b1c060ac0b7b3057f811a
Sergey Dolgikh <ageres7@gmail.com>
2020-11-07 22:06:08 +0500

    added class and screen

---
+++ HW2.8-pael/View Controllers/ResultViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ResultViewController.swift
+//  HW2.8-pael
+//
+//  Created by Сергей Долгих on 07.11.2020.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
